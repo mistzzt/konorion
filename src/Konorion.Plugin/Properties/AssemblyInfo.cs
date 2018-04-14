5803345738126b54b6bc5e1e6b41afe231c6a5dd
Ziteng Wang <mymisting@gmail.com>
2018-04-13 23:49:30 -0700

    Remove unused code

--- src/Konorion.Plugin/Properties/AssemblyInfo.cs
+++ src/Konorion.Plugin/Properties/AssemblyInfo.cs
@@ -19,6 +19,8 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+[assembly: InternalsVisibleTo("Konorion.ScriptTest")]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6f7f103e-19c5-4abb-aa7e-ffe1b6c28588")]
 
